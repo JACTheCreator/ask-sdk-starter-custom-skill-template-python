e33eb6535037e6a1a01b8102fdf1220537196638
Jermaine Coates <jermainecoates@outlook.com>
2020-02-17 01:29:10 -0500

    Made the first oder intent the next intent after launch

--- intents/launch_request_handler.py
+++ intents/launch_request_handler.py
@@ -2,9 +2,9 @@ from ask_sdk_core.dispatch_components import AbstractRequestHandler
 from ask_sdk_core.utils import is_request_type
 
 from utils.say import say
-# from utils.common import set_next_intent, set_prev_msg
+from utils.common import set_next_intent
 
-from constants.intents import (LAUNCH_REQUEST)
+from constants.intents import (LAUNCH_REQUEST, FIRST_ORDER_INTENT)
 
 class LaunchRequestHandler(AbstractRequestHandler):
     """Handler for Skill Launch."""
@@ -16,14 +16,12 @@ class LaunchRequestHandler(AbstractRequestHandler):
         # type: (HandlerInput) -> Response
 
         # Setting the next intent.
-        # set_next_intent(handler_input = handler_input, 
-        #                 next_intent = [GET_GUESS_ALEXA_NUMBER_INTENT, 
-        #                                GET_GUESS_MY_NUMBER_INTENT],
-        #                 is_launch_request = True)
+        set_next_intent(handler_input = handler_input, 
+                        next_intent = [FIRST_ORDER_INTENT],
+                        is_launch_request = True)
        
         # Welcoming the user.
         speech_text = say.welcome()
         reprompt_text = say.didnothear() + speech_text
-        # set_prev_msg(handler_input, msg = 'What mode would you like to play?')
         handler_input.response_builder.speak(speech_text).ask(reprompt_text).set_should_end_session(False)        
         return handler_input.response_builder.response
