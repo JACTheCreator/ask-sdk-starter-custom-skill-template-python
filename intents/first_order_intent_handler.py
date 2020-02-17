0a4b77876ca3568c335dbcf893a1cac7f9ebf911
Jermaine Coates <jermainecoates@outlook.com>
2020-02-17 01:26:36 -0500

    Add Intent handler for first order

---
+++ intents/first_order_intent_handler.py
@@ -0,0 +1,32 @@
+from ask_sdk_core.dispatch_components import AbstractRequestHandler
+from ask_sdk_core.utils import is_intent_name
+
+from constants.intents import FIRST_ORDER_INTENT, SECOND_ORDER_INTENT
+
+from utils.say import say
+
+from utils.common import (set_next_intent, is_next_intent_error, 
+                          handle_next_intent_error, set_prev_msg,
+                          get_prev_msg)
+
+class FirstOrderIntentHandler(AbstractRequestHandler):
+    """Handler for Help Intent."""
+    def can_handle(self, handler_input):
+        # type: (HandlerInput) -> bool
+        return is_intent_name(FIRST_ORDER_INTENT)(handler_input)
+
+    def handle(self, handler_input):
+        # type: (HandlerInput) -> Response
+
+        # Checking if this intent should play now.
+        current_intent = [FIRST_ORDER_INTENT]
+        if is_next_intent_error(handler_input = handler_input, current_intent = current_intent):
+            return handle_next_intent_error(handler_input = handler_input)   
+
+        # Setting the next intent.
+        set_next_intent(handler_input = handler_input, 
+                        next_intent = [SECOND_ORDER_INTENT])
+
+        speech_text = say.firstorder()
+        handler_input.response_builder.speak(speech_text).set_should_end_session(False)
+        return handler_input.response_builder.response
